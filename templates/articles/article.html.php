5e5d2ef8fb0c6e62af2e2122cdf265418cdec481
zaid83 <zaidchakir@yahoo.fr>
2023-04-10 17:00:22 +0200

    master

--- templates/articles/article.html.php
+++ templates/articles/article.html.php
@@ -18,36 +18,39 @@
                     <?= htmlspecialchars($article['date_article']) ?>
                 </span><br>
                 <span class="meta">
-                    <?php if ($check_like->rowCount() == 1) { ?>
-                        <a href="likes.php?type=1&id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:red" class="fa fa-heart" aria-hidden="true"></i>
-                            J'aime </a>
-                    <?php } else { ?>
-                        <a href="likes.php?type=1&id=<?= htmlspecialchars($article['id_article']) ?>"><i class="fa fa-heart" aria-hidden="true"></i>
-                            J'aime </a>
-                    <?php } ?>
-                    <?php if ($check_dislike->rowCount() == 1) { ?>
-                        <a href="likes.php?type=2&id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:blue" class="fa fa-thumbs-down" aria-hidden="true"></i>
-                            J'aime pas </a>
-                    <?php } else { ?>
-                        <a href="likes.php?type=2&id=<?= htmlspecialchars($article['id_article']) ?>"><i class="fa fa-thumbs-down" aria-hidden="true"></i>
-                            J'aime pas </a>
-                    <?php } ?>
-                    <?php if ($checkfav->rowCount() == 1) { ?>
-                        <a href="favourites.php?id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:red" class="fa fa-minus" aria-hidden="true"></i>
-                            <span style="color:red">Retirer des favoris</span>
+                    <?php
+                    if (isset($_SESSION['id'])) {
+                        if ($check_like->rowCount() == 1) { ?>
+                            <a href="index.php?controller=article&task=likeArticle&type=1&id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:red" class="fa fa-heart" aria-hidden="true"></i>
+                                J'aime </a>
                         <?php } else { ?>
-                            <a href="favourites.php?id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:green" class="fa fa-plus" aria-hidden="true"></i>
-                                <span style="color:green">Ajouter aux favoris</span>
-                            <?php } ?>
+                            <a href="index.php?controller=article&task=likeArticle&type=1&id=<?= htmlspecialchars($article['id_article']) ?>"><i class="fa fa-heart" aria-hidden="true"></i>
+                                J'aime </a>
+                        <?php } ?>
+                        <?php if ($check_dislike->rowCount() == 1) { ?>
+                            <a href="index.php?controller=article&task=likeArticle&type=2&id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:blue" class="fa fa-thumbs-down" aria-hidden="true"></i>
+                                J'aime pas </a>
+                        <?php } else { ?>
+                            <a href="index.php?controller=article&task=likeArticle&type=2&id=<?= htmlspecialchars($article['id_article']) ?>"><i class="fa fa-thumbs-down" aria-hidden="true"></i>
+                                J'aime pas </a>
+                        <?php } ?>
+                        <?php if ($checkfav->rowCount() == 1) { ?>
+                            <a href="index.php?controller=favourite&task=addOrRemove&id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:red" class="fa fa-minus" aria-hidden="true"></i>
+                                <span style="color:red">Retirer des favoris</span>
+                            <?php } else { ?>
+                                <a href="index.php?controller=favourite&task=addOrRemove&id=<?= htmlspecialchars($article['id_article']) ?>"><i style="color:green" class="fa fa-plus" aria-hidden="true"></i>
+                                    <span style="color:green">Ajouter aux favoris</span>
+                                <?php } ?>
 
-                            </a>
-                            <span><i class="fa fa-thumbs-up count"></i>
-                                <?= $likes ?>
-                            </span>
-                            <span><i class="fa fa-thumbs-down count"></i>
-                                <?= $dislikes ?>
-                            </span>
+                                </a>
+                                <span><i class="fa fa-thumbs-up count"></i>
+                                    <?= $likes ?>
+                                </span>
+                                <span><i class="fa fa-thumbs-down count"></i>
+                                    <?= $dislikes ?>
+                                </span>
                 </span>
+            <?php } ?>
             </div>
         </div>
     </div>
